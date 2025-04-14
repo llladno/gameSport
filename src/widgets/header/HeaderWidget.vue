734a50a39dd21e93904738b1443c5d9ec717867c
llladno <man30968@gmail.com>
2025-04-14 09:34:12 +0300

    feat: add headerWidget and LinkComponent

---
+++ src/widgets/header/HeaderWidget.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="flex gap-2">
+    <link-component to="/">Todo List</link-component>
+    <link-component to="/settings">Settings</link-component>
+    <link-component to="/about">About</link-component>
+  </header>
+</template>
+
+<script setup lang="ts">
+import {LinkComponent} from "@shared/ui";
+</script>
