2fb08b289ff0648ffdfeb6b6c018a729cf7f735b
llladno <man30968@gmail.com>
2025-04-14 09:34:22 +0300

    feat: add headerWidget and LinkComponent

---
+++ src/shared/ui/LinkComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <router-link :to="to" class="link">
+    <slot></slot>
+  </router-link>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  to: string
+}>()
+</script>
+
+<style scoped lang="scss">
+@use "@styles/variables";
+
+.link {
+  text-decoration: none;
+  color: variables.$primary-color;
+  cursor: pointer;
+  transition: color 0.2s ease-in-out;
+
+  &:hover {
+    color: variables.$primary-color-hover;
+  }
+}
+</style>
